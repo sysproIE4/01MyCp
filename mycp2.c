6c2929635f93f4dfa4068e3b02c57f4ccc1a7e8f
tctsigemura <sigemura@tokuyama.kosen-ac.jp>
2020-05-26 21:21:14 +0900

    書き込みopenのヒント追加

--- mycp2.c
+++ mycp2.c
@@ -10,7 +10,10 @@ int main(int argc, char *argv[]) {
   ssize_t len;                     // 実際に読んだバイト数
   char buf[BSIZ];                  // バッファ
 
-  // ここにプログラムを書き加えて完成させる．
+  // 以下にプログラムを書き加えて完成させる．
+  //
+  // ヒント：書き込みopenは次のようにすると良い．
+  // fd2 = open(argv[2], O_WRONLY|O_TRUNC|O_CREAT, 0644);
 
   close(fd1);
   close(fd2);
